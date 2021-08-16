53d42bafb3a9eb02e1f3a6381bf6ac4e1e801476
dherrerambo <38472789+dherrerambo@users.noreply.github.com>
2021-08-16 15:20:26 -0500

    Consumo de datos abiertos

---
+++ datos_abiertos.py
@@ -0,0 +1,39 @@
+from sodapy import Socrata
+import datetime
+import pandas as pd
+
+
+def get_meta_from_item(ds_item):
+    """
+        get metadata from dataset
+    """
+    cols = ['id','name','description']
+    meta = dict((k,v) for k,v in ds_item['resource'].items() if k in cols)
+    try:
+        t = ds_item['classification'].copy()
+        t.update(dict(list(a.values()) for a in t['domain_metadata']))
+        del t['domain_metadata']
+    except:
+        print(meta['id'])
+        pass
+        raise Exception('there is no [classification][domain_metadata]')
+    meta.update( t )
+    meta.update( dict((k,v) for k,v in ds_item['resource'].items() if k not in cols) )
+    return meta
+
+def get_metadata(type:str=None):
+    ## get datasets
+    with Socrata("www.datos.gov.co", None) as client:
+        data = client.datasets(limit=0, offset=0)
+    ## get meta
+    meta = [get_meta_from_item(x) for x in data]
+    metadata = pd.DataFrame(meta)
+    metadata['getdata_datetime'] = datetime.datetime.now()
+    if type!=None:
+        metadata = metadata[metadata['type']=='dataset'].reset_index(drop=True)
+    return metadata
+
+
+
+## main
+metadata = get_metadata()
