ef49d53dca7211c7658777ff6bcf61aff8306100
dherrerambo <38472789+dherrerambo@users.noreply.github.com>
2021-08-16 18:35:57 -0500

    Update datos_abiertos.py

--- datos_abiertos.py
+++ datos_abiertos.py
@@ -1,39 +1,32 @@
 from sodapy import Socrata
-import datetime
-import pandas as pd
 
-
-def get_meta_from_item(ds_item):
+def get_inventory()->dict:
     """
-        get metadata from dataset
+        the inventory can be parse as DataFrame with:
+            pd.DataFrame.from_dict(inventory, orient='index')
     """
-    cols = ['id','name','description']
-    meta = dict((k,v) for k,v in ds_item['resource'].items() if k in cols)
-    try:
-        t = ds_item['classification'].copy()
-        t.update(dict(list(a.values()) for a in t['domain_metadata']))
-        del t['domain_metadata']
-    except:
-        print(meta['id'])
-        pass
-        raise Exception('there is no [classification][domain_metadata]')
-    meta.update( t )
-    meta.update( dict((k,v) for k,v in ds_item['resource'].items() if k not in cols) )
-    return meta
-
-def get_metadata(type:str=None):
-    ## get datasets
+    def get_meta_from_item(ds_item:dict)->dict:
+        """
+            get metadata from dataset
+        """
+        cols = ['id','name','description']
+        meta = dict((k,v) for k,v in ds_item['resource'].items() if k in cols)
+        try:
+            t = ds_item['classification'].copy()
+            t.update(dict(list(a.values()) for a in t['domain_metadata']))
+            del t['domain_metadata']
+        except:
+            print(meta['id'])
+            pass
+            raise Exception('there is no [classification][domain_metadata]')
+        meta.update( t )
+        meta.update( dict((k,v) for k,v in ds_item['resource'].items() if k not in cols) )
+        return meta
+    
+    ## get data available
     with Socrata("www.datos.gov.co", None) as client:
-        data = client.datasets(limit=0, offset=0)
+        dataAvailable = client.datasets(limit=0, offset=0)
     ## get meta
-    meta = [get_meta_from_item(x) for x in data]
-    metadata = pd.DataFrame(meta)
-    metadata['getdata_datetime'] = datetime.datetime.now()
-    if type!=None:
-        metadata = metadata[metadata['type']==type].reset_index(drop=True)
-    return metadata
-
-
-
-## main
-metadata = get_metadata()
+    inventory = [get_meta_from_item(x) for x in dataAvailable]
+    inventory = dict((x['id'], dict((k,v) for k,v in x.items() if k!='id')) for x in inventory)
+    return inventory
